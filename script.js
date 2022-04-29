51a8fc09a75605a59952e764adfd523159e82a42
eddybayes <orangejamface@gmail.com>
2022-04-29 17:11:38 -0400

    removed some unnecessary css for now, may need this later as project progresses but for now its good to keep only functional code in

--- script.js
+++ script.js
@@ -6,9 +6,8 @@ function createEtchBoard() {        //new function to create divs inside contain
     for (i = 0; i < divAmount*divAmount; i ++ ) {
         let boardDiv = document.createElement("div");
         boardDiv.className = "gridDivs";
-
-        container.appendChild(boardDiv);
         boardDiv.style.cssText = `border: 1px dashed white; width: 10px; height: 10px;`;  //give each div a 10px by 10px size
+        container.appendChild(boardDiv);
   }};
 
     createEtchBoard();
