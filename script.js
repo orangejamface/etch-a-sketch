37ce2236ea78a7b0804f9dd656a1ea17063bc7b6
eddybayes <orangejamface@gmail.com>
2022-04-29 16:10:58 -0400

    created function to create etch a sketch board, it does so by calling a second function within it 16 times using a loop

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 const container = document.querySelector('#container'); //target container to place 16 divs within
 
-const div1 = document.createElement('div');
-div1.classList.add('div1');
-div1.textContent = '1';
+// const div1 = document.createElement('div');
+// div1.classList.add('div1');
+// div1.textContent = '1';
+
+
+function createDiv() {        //function to create a div 
+    let boardDiv = document.createElement("div");
+
+    boardDiv.className = "grid-o-Divs";
+    boardDiv.innerText = "1";
+    boardDiv.style.cssText = 'border: 2px dashed black;';
+    return boardDiv;   //return created div
+
+  }
+
+  function createBoard() {      //function to create 16 divs within div id='container', container exists in html
+    let container = document.getElementById("container"),  //target container div
+      myDivs = [];  //empty array for divs to be placed in
+      divAmount = 16;  //number of divs needed within container
+    
+    for (i = 0; i < divAmount; i += 1) {
+      myDivs.push(createDiv());
+      container.appendChild(myDivs[i]);  //place divs within div id='container'
+    }
+};
+
+    createBoard();
 
 
 
 
-container.appendChild(div1);
+
 
 
