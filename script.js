a788752dd8442e3d3bc7cad7c844227a4f4bc98a
eddybayes <orangejamface@gmail.com>
2022-04-29 15:39:42 -0400

    created index ,style amd script files, added container div to html, started by creating one div inside container using js

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector('#container'); //target container to place 16 divs within
+
+const div1 = document.createElement('div');
+div1.classList.add('div1');
+div1.textContent = '1';
+
+
+
+
+container.appendChild(div1);
+
+
