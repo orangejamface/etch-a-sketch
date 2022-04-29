969d20bb432f01598b477d5783f9fb3a0b15ebac
eddybayes <orangejamface@gmail.com>
2022-04-29 17:05:15 -0400

    altered js creating etch-a-sketch grid, simplified ot one function creating a grid of divs based on the number of divs specified in a variable

--- script.js
+++ script.js
@@ -1,32 +1,17 @@
 const container = document.querySelector('#container'); //target container to place 16 divs within
-
-// const div1 = document.createElement('div');
-// div1.classList.add('div1');
-// div1.textContent = '1';
+let divAmount = 16;  // the amount of divs
 
 
-function createDiv() {        //function to create a div 
-    let boardDiv = document.createElement("div");
+function createEtchBoard() {        //new function to create divs inside container on html
+    for (i = 0; i < divAmount*divAmount; i ++ ) {
+        let boardDiv = document.createElement("div");
+        boardDiv.className = "gridDivs";
 
-    boardDiv.className = "grid-o-Divs";
-    boardDiv.innerText = "1";
-    boardDiv.style.cssText = 'border: 2px dashed black;';
-    return boardDiv;   //return created div
+        container.appendChild(boardDiv);
+        boardDiv.style.cssText = `border: 1px dashed white; width: 10px; height: 10px;`;  //give each div a 10px by 10px size
+  }};
 
-  }
-
-  function createBoard() {      //function to create 16 divs within div id='container', container exists in html
-    let container = document.getElementById("container"),  //target container div
-      myDivs = [];  //empty array for divs to be placed in
-      divAmount = 16;  //number of divs needed within container
-    
-    for (i = 0; i < divAmount; i += 1) {
-      myDivs.push(createDiv());
-      container.appendChild(myDivs[i]);  //place divs within div id='container'
-    }
-};
-
-    createBoard();
+    createEtchBoard();
 
 
 
