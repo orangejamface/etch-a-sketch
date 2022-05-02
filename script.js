3a10608b031864d7866bee853303e3303bdb05fc
eddybayes <orangejamface@gmail.com>
2022-05-02 11:20:49 -0400

    added event listener to grid, upon mouse over of any element within the grid a function it run to change the background color of where mouse hovers, this change is permanent

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 const container = document.querySelector('#container'); //target container to place 16 divs within
 let divAmount = 16;  // the amount of divs
+let square = document.querySelector('.gridDivs')
 
 
-function createEtchBoard() {        //new function to create divs inside container on html
+function changeColor (e) {    //simple function to change background color, function will be called when mouseove event occurs
+  e.target.style.backgroundColor = '#8a2be2';
+}
+
+function createEtchBoard() {        //function to create divs inside container div
     for (i = 0; i < divAmount*divAmount; i ++ ) {
         let boardDiv = document.createElement("div");
         boardDiv.className = "gridDivs";
-        boardDiv.style.cssText = `border: 1px dashed white; width: 10px; height: 10px;`;  //give each div a 10px by 10px size
+        boardDiv.style.cssText = `border: 1px solid white; width: 40px; height: 40px;`;  //give each div a 10px by 10px size
         container.appendChild(boardDiv);
-  }};
+    };
+};
 
     createEtchBoard();
-
-
-
-
+    container.addEventListener('mouseover', changeColor) 
 
 
 
